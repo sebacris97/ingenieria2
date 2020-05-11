0998bd7fc26f03ef312d267ff858c19b8fc66a64
sebacris97 <51713904+sebacris97@users.noreply.github.com>
2020-05-11 06:04:27 -0300

    mejorado el sitio admin y modelo de novedades

--- bookflix/bookflixapp/models.py
+++ bookflix/bookflixapp/models.py
@@ -10,12 +10,18 @@ class Autor(models.Model):
     def __str__(self):
         return '%s %s' % (self.apellido, self.nombre)
 
+    class Meta:
+        verbose_name_plural = "Autores"
+
 
 class Genero(models.Model):
     nombre = models.CharField(max_length=25)
 
     def __str__(self):
         return self.nombre
+    
+    class Meta:
+        verbose_name_plural = "Generos"
 
 
 class Editorial(models.Model):
@@ -24,21 +30,37 @@ class Editorial(models.Model):
     def __str__(self):
         return self.nombre
 
+    class Meta:
+        verbose_name_plural = "Editoriales"
+
 
 class Libro(models.Model):
     titulo = models.CharField(max_length=200)
-    nropaginas = models.PositiveIntegerField()
-    nrocapitulos = models.PositiveIntegerField()
-    isbn = models.CharField(max_length=13)
+    nropaginas = models.PositiveIntegerField(verbose_name="Numero de paginas")
+    nrocapitulos = models.PositiveIntegerField(verbose_name="Numero de capitulos")
+    isbn = models.CharField(max_length=13,verbose_name="ISBN")
     autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
     editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
     genero = models.ManyToManyField(Genero)
-    agnoedicion = models.DateField()
+    agnoedicion = models.DateField(verbose_name="Año de edicion")
 
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        verbose_name_plural = "Libros"
+
+
+        
 
 class Novedad(models.Model):
     titulo = models.CharField(max_length=200)
     texto = models.TextField()
+    creada = models.DateField(verbose_name="Creacion")
 
     def __str__(self):
         return self.titulo
+
+    class Meta:
+        verbose_name_plural = "Novedades"
