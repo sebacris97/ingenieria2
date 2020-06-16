7fc99a6ef5a3040b0cbc72fe00fe11e322a3093f
argyulo <62069571+argyulo@users.noreply.github.com>
2020-06-16 19:43:02 -0300

    Se agrego filtro para ver libros

--- bookflix/bookflixapp/views.py
+++ bookflix/bookflixapp/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import hashers
 from django.contrib.auth import login as do_login
 from .forms import RegistrationForm, CreateProfileForm
+from .filters import LibroFilter
 
 # from .forms import FormularioAgregarLibro
 
@@ -41,8 +42,8 @@ def agregar_libro(request):
 
 
 def ver_libros(request):
-    libros = Libro.objects.all()
-    return render(request, "ver_libros.html", {"libros": libros})
+    filtro = LibroFilter(request.GET, queryset=Libro.objects.all())
+    return render(request, "ver_libros.html", {"filter": filtro})
 
 
 def ver_capitulos(request, pk):
